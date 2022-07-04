3547775cbab7deb605a3d41948590d7d1352e2e4
danatrace <89346509+danatrace@users.noreply.github.com>
2022-07-04 22:22:07 +0200

    Update create_easytravel.sh

--- scripts/create_easytravel.sh
+++ scripts/create_easytravel.sh
@@ -6,4 +6,4 @@ kubectl apply -f ../manifests/easytravel-config.yaml
 kubectl apply -f ../manifests/easytravel-mongodb.yaml
 kubectl apply -f ../manifests/easytravel-backend.yaml
 kubectl apply -f ../manifests/easytravel-frontend.yaml
-kubectl apply -f ../manifests/easytravel-nginx.yaml
+#kubectl apply -f ../manifests/easytravel-nginx.yaml
